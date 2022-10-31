8d74497fefefa27abb38b127ef855d2e655a202f
gull2365 <jych2365@gmail.com>
2022-10-31 16:03:50 +0900

    feat: Subscriptions

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.getElementById("span");
+
+const countModifier = (count = 0, action) => {
+    if(action.type === "ADD"){
+        return count +1;
+    }else if(action.type === "MINUS"){
+        return count -1;
+    }else {
+        return count;
+    }
+};
+
+const countStore = createStore(countModifier);
+
+const handleAdd = () =>{
+    countStore.dispatch({type:"ADD"})
+}
+
+const handleMinus = () =>{
+    countStore.dispatch({type:"MINUS"})
+}
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
